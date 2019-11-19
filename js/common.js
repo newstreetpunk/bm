b8658ebfb3b3f92e68c2a20d7fda5ea01f12a627
newstreetpunk <newstreetpunk@yandex.ru>
2019-11-19 23:00:01 +0500

    123

--- js/common.js
+++ js/common.js
@@ -1,3 +1,33 @@
-$(function() {
+jQuery(function($) {
+
+	// Кнопка меню
+	$('.mobile-btn').on('click', function() {
+		$(this).toggleClass('active');
+	});
+	//
+
+	//Высчитываем и добавляем отступ слева при ресайзе слайдеру на главной
+	function resize () {
+		var ww = $(window).width(),
+			wWrap = $('.wrapper').width(),
+			pl = (ww - wWrap) / 2;
+
+		$('.hero__slider').css('padding-left', pl)
+	}
+	resize ();
+
+	$(window).resize(function(){
+		resize ();
+	});
+	//
+
+	// Слик слайдер
+	$('.hero__slider').slick({
+		autoplay: true,
+		autoplaySpeed: 5000,
+		arrows: false,
+		dots: true
+	});
+
 
 });
