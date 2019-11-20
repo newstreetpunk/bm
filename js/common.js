d0b4d94b24de7f17f303059ad9512d6e3438c351
newstreetpunk <newstreetpunk@yandex.ru>
2019-11-20 20:54:01 +0500

    Mobail header

--- js/common.js
+++ js/common.js
@@ -1,5 +1,67 @@
 jQuery(function($) {
 
+	//Видео слайдер
+	$('.video__slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		prevArrow: '<button class="slick-prev"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"	 viewBox="0 0 477.175 477.175" xml:space="preserve">	<path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225 c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"/></svg></button>',
+		nextArrow: '<button class="slick-next"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"	 viewBox="0 0 477.175 477.175" xml:space="preserve">	<path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225 c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"/></svg></button>',
+		responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: false,
+				dots: true
+			}
+		}
+		]
+	});
+	//
+
+	// Video Load & Popup
+	$('.video-popup').magnificPopup({
+		disableOn: 700,
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: true,
+
+		fixedContentPos: true,
+		type: 'iframe',
+		iframe: {
+			markup: '<div class="mfp-iframe-scaler">'+
+			'<div class="mfp-close"></div>'+
+			'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+			'</div>',
+			patterns: {
+				youtube: {
+					index: 'youtube.com/', 
+					id: 'v=',
+					src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+				},
+				vimeo: {
+					index: 'vimeo.com/',
+					id: '/',
+					src: '//player.vimeo.com/video/%id%?autoplay=1'
+				},
+				gmaps: {
+					index: '//maps.google.',
+					src: '%id%&output=embed'
+				}
+			},
+			srcAction: 'iframe_src', 
+		}
+	});
+	//
+
 	// Применение CSS шапке при скролле
 	if( $(window).width() > 580 ){
 		$(window).scroll(function () {
@@ -25,7 +87,7 @@ jQuery(function($) {
 	});
 	//
 
-	//Высчитываем и добавляем отступ слева при ресайзе
+	//Высчитываем при ресайзе
 	function resize () {
 		var ww = $(window).width(),
 			wWrap = $('.wrapper').width(),
@@ -33,8 +95,11 @@ jQuery(function($) {
 
 		$('.hero__slider').css('padding-left', pl);
 
-		var itemW= $('.my-grid__item').width();
-		$('.my-grid__item .img-block, .my-grid__item .text-block').css('height', itemW)
+		if ( ww > 580 ){
+			var itemW= $('.my-grid__item').width();
+			$('.my-grid__item .img-block, .my-grid__item .text-block').css('height', itemW);
+		}
+		
 	}
 	resize ();
 
